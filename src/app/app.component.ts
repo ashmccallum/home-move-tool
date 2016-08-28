aea143b2ef0ebbe66b391fa4986f20ed51c104dc
ashmccallum <ashm@outlook.com>
2016-08-28 22:46:11 +0100

    angular2-material modules loaded and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { MdCheckbox } from '@angular2-material/checkbox';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MdCheckbox]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Someone told me: app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { MdCheckboxModule } from '@angular2-material/checkbox';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdInputModule } from '@angular2-material/input';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +18,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    MdButtonModule,
+    MdSidenavModule,
+    MdCheckboxModule,
+    MdIconModule,
+    MdInputModule
   ],
   providers: [],
   entryComponents: [AppComponent],
